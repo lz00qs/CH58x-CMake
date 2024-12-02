632e0b2219d40efc0e9959c34c91f9f33014ac39
lz00qs <lz00qs@gmail.com>
2024-12-02 23:55:16 +0800

    feat: add printf support for UART0 and add blink for PA8

--- APP/main.c
+++ APP/main.c
@@ -1,24 +1,24 @@
 #include "CH58x_common.h"
 #include "stdio.h"
-#define LED_Pin GPIO_Pin_15
+#define LED_Pin GPIO_Pin_8
 
 int main()
 {
     SetSysClock(CLK_SOURCE_PLL_60MHz);
-    GPIOB_ModeCfg(LED_Pin, GPIO_ModeOut_PP_5mA);
+    GPIOA_ModeCfg(LED_Pin, GPIO_ModeOut_PP_5mA);
 
-    // for UART1 init
-    GPIOA_SetBits(GPIO_Pin_9);
-    GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);      // RXD-配置上拉输入
-    GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA); // TXD-配置推挽输出，注意先让IO口输出高电平
-    UART1_DefInit();
+    // for UART0 initialization
+    GPIOB_SetBits(GPIO_Pin_7);
+    GPIOB_ModeCfg(GPIO_Pin_4, GPIO_ModeIN_PU);      // RXD-配置上拉输入
+    GPIOB_ModeCfg(GPIO_Pin_7, GPIO_ModeOut_PP_5mA); // TXD-配置推挽输出，注意先让IO口输出高电平
+    UART0_DefInit();
 
     while (1)
     {
         printf("Hello World!\n");
-        GPIOB_SetBits(LED_Pin);
+        GPIOA_SetBits(LED_Pin);
         DelayMs(1000);
-        GPIOB_ResetBits(LED_Pin);
+        GPIOA_ResetBits(LED_Pin);
         DelayMs(1000);
     }
 }
